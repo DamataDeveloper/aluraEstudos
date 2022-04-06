d9887be495df02191ec80bab52d76d59df26745b
DamataVeloper <damatadeveloper@gmail.com>
2022-04-06 15:46:34 -0300

    criando tabela np banco de dados via script

--- backEnd/JulianaNegreiros/RestComNodeJS-APIcomExpress-e-MySQL/index.js
+++ backEnd/JulianaNegreiros/RestComNodeJS-APIcomExpress-e-MySQL/index.js
@@ -1,12 +1,14 @@
 const customExpress = require('./config/customExpressConfig');
 const conexao = require('./infraetrutura/conexaoInfraestrutura');
-
+const TabelasBanco = require('./infraetrutura/tabelasInfraestrutura');
 
 conexao.connect((erro)=>{
     if(erro){
         console.log(erro)
     }else{
         console.log('conectado com o banco de dados');
+
+        TabelasBanco.init(conexao);
         const app = customExpress();
         app.listen(3000, ()=> console.log('servidor rodando na porta 3000'));
     }

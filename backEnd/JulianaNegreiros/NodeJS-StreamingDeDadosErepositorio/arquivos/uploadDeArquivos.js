7bb2d86db42eeb7a9bda12d1f52a2f6ff52705e3
DamataVeloper <damatadeveloper@gmail.com>
2022-04-07 13:08:38 -0300

    agora manipulando imgem com streams

--- backEnd/JulianaNegreiros/NodeJS-StreamingDeDadosErepositorio/arquivos/uploadDeArquivos.js
+++ backEnd/JulianaNegreiros/NodeJS-StreamingDeDadosErepositorio/arquivos/uploadDeArquivos.js
@@ -1,10 +1,15 @@
 const fs = require('fs');
-
+/*
 fs.readFile('./assets/lua4.jpg', (error, buffer)=>{
     console.log('imagem bufferizada');
     console.log(buffer)
 
     fs.writeFile('./assets/lua5.jpg', buffer, erro => {
-        console.log('imagem escrita')
+        console.log('imagem escrita') 
     })
 })
+*/
+fs.createReadStream('./assets/lua4.jpg') 
+    .pipe(fs.createWriteStream('./assets/lua-stream.jpg'))
+    .on('finish', ()=> console.log('imagem stream escrita com sucesso'))
+

4e22d90c5bda88acd081d6ccd89e20663682d6a3
DamataVeloper <damatadeveloper@gmail.com>
2022-04-08 16:51:11 -0300

    crud com express usando array livros como estudo

--- backEnd/JacquelineOliveira/alura-node/src/app.js
+++ backEnd/JacquelineOliveira/alura-node/src/app.js
@@ -19,10 +19,31 @@ app.get('/', (req, res)=>{
 app.get('/livros', (req, res)=>{
     res.status(200).json(livros)
 })
+app.get('/livros/:id',(req, res)=>{
+    let index =  buscaLivro(req.params.id);    
+    res.status(200).json(livros[index])
+})
 app.post('/livros',(req, res)=>{
     livros.push(req.body)
     res.status(201).json(livros)
 })
+app.put('/livros/:id',(req, res)=>{
+    let index =  buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.status(200).json(livros[index])
+})
+app.delete('/livros/:id',(req, res)=>{
+    let {id} = req.params; //atribuicao via disestruturacao
+    let index =  buscaLivro(id);
+    livros.splice(index, 1);
+    res.status(200).send(`Livro ${id} excluido com sucesso`)
+})
+
+
+function buscaLivro(id){
+    
+    return livros.findIndex(livro => livro.id == id)
+}
 
 
 export default app

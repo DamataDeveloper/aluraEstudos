df0f0770614a5474a9217a7b4551abab5385eea2
DamataVeloper <damatadeveloper@gmail.com>
2022-04-11 16:50:19 -0300

    criando e populando tabela

---
+++ backEnd/JulianaAmoasei/ORMcomNodeJS-APIcomSequeliza-e-MySQL/api/seeders/20220411172511-demo-pessoa.js
@@ -0,0 +1,57 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    
+    await queryInterface.bulkInsert('Pessoas', [
+      {
+        nome: 'John Doe',
+        ativo: true,
+        email: 'john@jonh.com',
+        role: 'estudante',
+        createAt: new Date(),
+        updateAt: new Date()      
+      },
+      {
+        nome: 'marcelo Doe',
+        ativo: true,
+        email: 'marcelo@jonh.com',
+        role: 'estudante',
+        createAt: new Date(),
+        updateAt: new Date()      
+      },
+      {
+        nome: 'catarina Doe',
+        ativo: true,
+        email: 'catarina@jonh.com',
+        role: 'estudante',
+        createAt: new Date(),
+        updateAt: new Date()      
+      },
+      {
+        nome: 'marlene Doe',
+        ativo: true,
+        email: 'marlene@jonh.com',
+        role: 'estudante',
+        createAt: new Date(),
+        updateAt: new Date()      
+      },
+      {
+        nome: 'marcos Doe',
+        ativo: true,
+        email: 'marcos@jonh.com',
+        role: 'estudante',
+        createAt: new Date(),
+        updateAt: new Date()      
+      },
+    ], 
+      {});
+    
+  },
+
+  async down (queryInterface, Sequelize) {
+     
+    await queryInterface.bulkDelete('People', null, {});
+    
+  }
+};

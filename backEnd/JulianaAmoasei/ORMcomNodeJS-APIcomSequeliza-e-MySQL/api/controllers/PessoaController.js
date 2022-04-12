defb15f9e84c7a8f1efae011ccfe4bb54a02f97e
DamataVeloper <damatadeveloper@gmail.com>
2022-04-12 12:56:09 -0300

    arrumando rota get pegaumpessoa problema no metodo findOne trocado por findByPk

--- backEnd/JulianaAmoasei/ORMcomNodeJS-APIcomSequeliza-e-MySQL/api/controllers/PessoaController.js
+++ backEnd/JulianaAmoasei/ORMcomNodeJS-APIcomSequeliza-e-MySQL/api/controllers/PessoaController.js
@@ -14,11 +14,22 @@ class PessoaController{
         const id = req.params.id
         console.log(id)
         try{            
-            const UmaPessoas = await database.Pessoas.findOne({
+            const UmaPessoas = await database.Pessoas.findByPk({
                 whrere:{
                     id: Number(id) 
                 }
             });            
+            return res.status(201).json(UmaPessoas)
+
+        }catch(error){
+            return res.status(500).json(error.message)
+        }
+    } 
+    static async pegaUmaPessoa(req, res){
+        const id = req.params.id
+        console.log(id)
+        try{            
+            const UmaPessoas = await database.Pessoas.findByPk(id)                        
             return res.status(201).json(UmaPessoas)
 
         }catch(error){
--- backEnd/JulianaAmoasei/ORMcomNodeJS-APIcomSequeliza-e-MySQL/api/routes/pessoasRoute.js
+++ backEnd/JulianaAmoasei/ORMcomNodeJS-APIcomSequeliza-e-MySQL/api/routes/pessoasRoute.js
@@ -5,7 +5,8 @@ const PessoaController = require('../controllers/PessoaController')
 
 const router = Router()
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
-router.get('/pessoas/:id', PessoaController.pegaUmaPessoas)
+//router.get('/pessoas/:id', PessoaController.pegaUmaPessoas)
+router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
 router.post('/pessoas', PessoaController.criaPessoa)
 
 

0ce9462fe45e9f9282156e167912d72319188bfb
DamataVeloper <damatadeveloper@gmail.com>
2022-04-14 17:06:43 -0300

    montando mais uma tabela atraves do sequelize

---
+++ backEnd/JulianaAmoasei/ORMcomNodeJS-AvancandoNasFuncionalidadesDoSequelize/api/migrations/20220414000001-addcolumn-pessoas.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('Pessoas', 'deletedAt', {
+      allowNull: true,     
+      type: Sequelize.DATE
+    })
+  },
+  down: (queryInterface) => {
+    return queryInterface.removeColumn('Pessoas', 'deletedAt')
+  }
+}

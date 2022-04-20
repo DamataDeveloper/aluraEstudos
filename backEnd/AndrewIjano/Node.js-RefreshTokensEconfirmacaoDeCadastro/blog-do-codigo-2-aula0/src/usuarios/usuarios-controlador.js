d09eb00d15ea4e579449a826664c61f3f2d5ed23
DamataVeloper <damatadeveloper@gmail.com>
2022-04-20 19:26:41 -0300

    usando redis para validar o allowlist

--- backEnd/AndrewIjano/Node.js-RefreshTokensEconfirmacaoDeCadastro/blog-do-codigo-2-aula0/src/usuarios/usuarios-controlador.js
+++ backEnd/AndrewIjano/Node.js-RefreshTokensEconfirmacaoDeCadastro/blog-do-codigo-2-aula0/src/usuarios/usuarios-controlador.js
@@ -3,6 +3,8 @@ const { InvalidArgumentError } = require('../erros');
 
 const jwt = require('jsonwebtoken');
 const blocklist = require('../../redis/manipula-blocklist');
+const allowlistRefreshToken = require('../../redis/allowlist-refresh-token')
+
 const crypto = require('crypto');
 const moment = require('moment');
 
@@ -18,7 +20,8 @@ function criaTokenJWT(usuario) {
 function criaTokenOpaco(usuario){
   const tokenOpaco = crypto.randomBytes(24).toString('hex');
   const dataExpiracao = moment().add(5, 'd').unix();
-  
+  await allowlistRefreshToken.adiciona(tokenOpaco, usuario.id, dataExpiracao);
+
   return tokenOpaco
 }
 
@@ -50,7 +53,7 @@ module.exports = {
   async login(req, res) {
     try {
       const accessToken = criaTokenJWT(req.user);
-      const refreshToken = criaTokenOpaco(req.user);
+      const refreshToken = await criaTokenOpaco(req.user);
       res.set('Authorization', accessToken);
       res.status(200).json({refreshToken});
     } catch (erro) {
